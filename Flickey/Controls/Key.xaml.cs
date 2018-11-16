19b9c9eaf5a46b2a401e9a875db601950e4002da
aridai <aridai.net@outlook.jp>
2018-11-16 15:29:03 +0900

    KeyShape列挙体を追加した。

--- Flickey/Controls/Key.xaml.cs
+++ Flickey/Controls/Key.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Flickey.Controls
@@ -8,6 +9,21 @@ namespace Flickey.Controls
     /// </summary>
     public partial class Key : UserControl
     {
+        /// <summary>
+        /// Shapeプロパティの依存関係プロパティ。
+        /// </summary>
+        public static DependencyProperty ShapeProperty
+            = DependencyProperty.Register(nameof(Shape), typeof(KeyShape), typeof(Key), new PropertyMetadata(KeyShape.Normal));
+
+        /// <summary>
+        /// キーの形を取得・設定します。
+        /// </summary>
+        public KeyShape Shape
+        {
+            get => (KeyShape)this.GetValue(ShapeProperty);
+            set => this.SetValue(ShapeProperty, value);
+        }
+
         public Key()
         {
             InitializeComponent();
---
+++ Flickey/Controls/KeyShape.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Flickey.Controls
+{
+    /// <summary>
+    /// キーの形を列挙します。
+    /// </summary>
+    public enum KeyShape
+    {
+        None,
+        Normal,
+        SlideLeft,
+        SlideRight,
+        SlideUp,
+        SlideDown,
+        Hold,
+        HoldLeft,
+        HoldRight,
+        HoldUp,
+        HoldDown
+    }
+}
