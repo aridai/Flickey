246f97d7fa8bfc2836ce655562236d01c479812d
aridai <aridai.net@outlook.jp>
2018-11-29 12:38:31 +0900

    Prismをインストールした。

---
+++ Flickey/Behaviors/MainWindowSetupBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Interactivity;
+
+namespace Flickey.Behaviors
+{
+    public sealed class MainWindowSetupBehavior : Behavior<Window>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+        }
+    }
+}
