2f23810d924a537b6f209072ee7397e1f7c1d5be
aridai <aridai.net@outlook.jp>
2018-11-30 12:48:24 +0900

    入力まわりの機能を仮実装した。

---
+++ Flickey/Models/InputMode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Flickey.Models
+{
+    /// <summary>
+    /// IMEの入力モードを列挙します。
+    /// </summary>
+    public enum InputMode
+    {
+        /// <summary>
+        /// IMEが無効です。
+        /// </summary>
+        Disabled,
+
+        /// <summary>
+        /// 直接入力モード。
+        /// </summary>
+        DirectInput,
+
+        /// <summary>
+        /// 日本語入力モード。
+        /// </summary>
+        JapaneseInput,
+
+        /// <summary>
+        /// 不明な入力モード。
+        /// </summary>
+        Unknown
+    }
+}
---
+++ Flickey/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace Flickey.ViewModels
+{
+    public sealed class MainWindowViewModel : BindableBase
+    {
+        public DelegateCommand<string> Command { get; }
+
+        public MainWindowViewModel()
+        {
+
+
+            Command = new DelegateCommand<string>(character =>
+            {
+                System.Diagnostics.Debug.WriteLine($"入力:{character}");
+
+                //Models.KeyboardOperator.InputDirectly("おっぱい");
+
+                //  スキャンコードを設定すると死ぬやつがあるっぽい?
+                //Models.KeyboardOperator.InputKey(Models.VirtualKeyCode.Kanji);
+
+                //Models.KeyboardOperator.InputKey(new Models.VirtualKeyCode[] { Models.VirtualKeyCode.Shift, Models.VirtualKeyCode.A });
+
+                //Models.KeyboardOperator.SetInputMode(Models.InputMode.JapaneseInput);
+            });
+        }
+    }
+}
